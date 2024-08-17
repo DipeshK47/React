56c4897702d9c87ab6f0559e990aa7fa50a61825
Dipesh Kumar <itsdipeshk2003@gmail.com>
2024-08-18 00:41:05 +0530

    megablog_project

--- megaBlog/src/components/Input.jsx
+++ megaBlog/src/components/Input.jsx
@@ -1,25 +1,24 @@
-import React , {useId} from "react";
+import React, {useId} from 'react'
 
-const Input = React.forwardRef( function Input(
-    {label,
+const Input = React.forwardRef( function Input({
+    label,
     type = "text",
-    className = '',
-    ...props}, 
-    ref
-){
+    className = "",
+    ...props
+}, ref){
     const id = useId()
     return (
-        <div className="w-full">
-             {label && <label 
+        <div className='w-full'>
+            {label && <label 
             className='inline-block mb-1 pl-1' 
             htmlFor={id}>
                 {label}
             </label>
             }
-            <input 
-            type="text"
+            <input
+            type={type}
             className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-            ref = {ref}
+            ref={ref}
             {...props}
             id={id}
             />
