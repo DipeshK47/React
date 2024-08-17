a4f46d2e254ef3230c5800fc8d91434fa75cafaa
Dipesh Kumar <itsdipeshk2003@gmail.com>
2024-08-17 19:28:06 +0530

    megaBlog

---
+++ megaBlog/src/components/index.js
@@ -0,0 +1,11 @@
+import Header from "./Header/Header"
+import Container from './container/Container'
+import Logo from "./Logo"
+import Footer from "./Footer/Footer"
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo
+}
